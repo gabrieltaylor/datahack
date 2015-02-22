d8053b5c472ecb1e4f7d6222017eff15192671b2
Greg Laughlin <greg.laughlin81@hotmail.com>
2015-02-21 22:45:41 -0800

    add InAppBrowser

--- in_season/platforms/android/assets/www/cordova_plugins.js
+++ in_season/platforms/android/assets/www/cordova_plugins.js
@@ -1,9 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.geolocation": "0.3.12"
+    "org.apache.cordova.geolocation": "0.3.12",
+    "org.apache.cordova.inappbrowser": "0.6.0"
 }
 // BOTTOM OF METADATA
 });
